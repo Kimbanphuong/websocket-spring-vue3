6130ac8a329984e350e75e5bec1f08d335d45b0f
Banphuong <ludev.fullstack@gmail.com>
2024-02-25 09:46:30 +0100

    Initial Commit

---
+++ src/test/java/com/banphuong/dev/websocketjavavue3/WebsocketJavaVue3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banphuong.dev.websocketjavavue3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketJavaVue3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
